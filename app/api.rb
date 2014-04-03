697abf2e59bb8f1067e3d6337e0b0c462b3123c7
Tim Cook <tim@wakeless.net>
2014-04-04 00:41:48 +1100

    added item endpoints

--- app/api.rb
+++ app/api.rb
@@ -16,11 +16,28 @@ class API < Grape::API
   resource :user do
     post 'new' do
       user = User.new
+      Rails.logger.info "params #{json_params}"
       service = UserService.new(user).update(json_params.require(:user).permit(:first_name, :last_name, :email))
       
       Rails.logger.info "params #{json_params[:user][:first_name]}"
       Rails.logger.info "params #{json_params[:user][:last_name]}"
       Rails.logger.info "params #{json_params[:user][:email]}"
+
+      if service.save
+        status 201
+        { "user created" => "first_name=#{user.first_name} last_name=#{user.last_name} email=#{user.email}" }
+      else
+        status 400
+      end
+    end
+  end
+
+  resource :item do
+    post 'new' do
+      item = Item.new
+      service = ItemService.new(item).update(json_params.require(:item).permit(:name))
+      
+      Rails.logger.info "params #{json_params[:item]}"
 
       if service.save
         status 201
